a82674d04ca0f66884efd8a25cd78709de1f419c
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-04 22:47:51 +0300

    solution

--- 01-pillars/index.js
+++ 01-pillars/index.js
@@ -22,36 +22,59 @@ Ipucu: setTimeout
 */
 
 class Bookshelf {
-    isFavoriteBook(bookName) {
-        // Kitabın içinde "JS" geçiyorsa favoridir.
+  constructor(apiUrl) {
+    this.apiURL = apiUrl;
+    this.books = [];
+    this.loadBooks();
+  }
+
+  isFavoriteBook(bookName) {
+    // Kitabın içinde "JS" geçiyorsa favoridir.
+    return String(bookName).includes("JS");
+  }
+
+  getFavoriteBooks() {
+    const length = this.books.length;
+    if (length === 0) {
+      console.log("Kitaplar henuz yuklenmedi.");
+      return [];
     }
+    console.log("Favorite kitaplariniz;");
 
+    const favoriteBooks = this.books.filter((currentBookName) =>
+      this.isFavoriteBook(currentBookName)
+    );
+    favoriteBooks.forEach((favoriteBookName) => {
+      console.log(favoriteBookName);
+    });
+    return favoriteBooks;
+  }
+
+  loadBooks() {
+    getBooksFromFakeAPI(this.apiURL, (books) => {
+      this.books = books;
+    });
+  }
 }
 
-
-function getFavoriteBooks() {
-    console.log('favori kitaplarınız;');
-}
-
-function loadBooks() {
-
-}
-
-// benimkitapligim = ...
-// benimkitapligim.getFavoriteBooks()
-setTimeout(function() { benimkitapligim.getFavoriteBooks() }, 301);
-var apiURL = 'somefake.url/getBooks';
+const BILIM_KURGU_KITAPLARI_API = "somefake.url/getBooks";
+// const BILISIM_KITAPLARI_API = 'somefake.url/getBooks';
+const benimKitapligim = new Bookshelf(BILIM_KURGU_KITAPLARI_API);
+benimKitapligim.getFavoriteBooks();
+setTimeout(() => {
+  benimKitapligim.getFavoriteBooks();
+}, 301);
 
 /* Lütfen buradan aşağısını değiştirmeyin  */
 function getBooksFromFakeAPI(apiURL, onBooksLoad) {
-    setTimeout(function() {
-        onBooksLoad([
-            "Otostopçunun galaksi rehberi",
-            "Albaya mektup yok",
-            "You don't know JS",
-            "JS: the good parts",
-            42, // Lets imagine we have corrupted data in back-end
-            false // Lets imagine we have corrupted data in back-end
-        ]);
-    }, 300);
+  setTimeout(function () {
+    onBooksLoad([
+      "Otostopçunun galaksi rehberi",
+      "Albaya mektup yok",
+      "You don't know JS",
+      "JS: the good parts",
+      42, // Lets imagine we have corrupted data in back-end
+      false, // Lets imagine we have corrupted data in back-end
+    ]);
+  }, 300);
 }
--- 02-cokpara/index.js
+++ 02-cokpara/index.js
@@ -1,1 +1,163 @@
-console.log('mounted');
+let maxPara = Number.MAX_SAFE_INTEGER;
+
+const productList = [
+  {
+    id: 1,
+    name: "Kola",
+    price: 4,
+    img: "https://i.sozcu.com.tr/wp-content/uploads/2016/01/19/diyet-kola.jpg",
+  },
+  {
+    id: 2,
+    name: "Iskender",
+    price: 30,
+    img: "https://i.lezzet.com.tr/images-xxlarge-recipe/ev-yapimi-iskender-33bd7089-fa36-4398-95f8-02c6463ea27c.jpg",
+  },
+  {
+    id: 3,
+    name: "Yat",
+    price: 450000,
+    img: "https://i.ytimg.com/vi/9BCZpcgsAb8/maxresdefault.jpg",
+  },
+  {
+    id: 4,
+    name: "Bahceli Ev",
+    price: 9500000,
+    img: "https://www.neredekal.com/res/blog/1582812421_7.jpg",
+  },
+  {
+    id: 5,
+    name: "Araba Fabrikası",
+    price: 120000000,
+    img: "https://i.ytimg.com/vi/rfMkp55oTv0/maxresdefault.jpg",
+  },
+  {
+    id: 6,
+    name: "Atımsı",
+    price: 2000,
+    img: "https://i.pinimg.com/originals/67/60/98/676098ce8d71dffb37f858c5807647bb.jpg",
+  },
+  {
+    id: 7,
+    name: "COVID Aşısı",
+    price: 4000,
+    img: "https://www.cumhuriyet.com.tr/Archive/2021/7/4/1849654/kapak_123409.jpg",
+  },
+  {
+    id: 8,
+    name: "VOLVO V90",
+    price: 90000,
+    img: "http://img03.platesmania.com/171005/o/10499245.jpg",
+  },
+  // ... Kendi örneklerinizi eklemeye çekinmeyin.
+];
+
+//Headerda para bolumunu guncelle
+function headerParaRender() {
+  document.getElementById("kalanPara").textContent = maxPara;
+}
+
+function urunOlustur({ id, name, price, img }) {
+  //Verilen data ile urun olustur.
+  const urunElement = `<div class="urun id=${id}">
+  <div class="urun__resim-container">
+    <img
+      src="${img}"
+      alt="${name}"
+      class="urun__resim"
+      id="urunResim"
+    />
+    <p class="urun__text urun__alinabilecek">
+      Alabilecegin: <span id="urunAlinabilecek">${Math.floor(
+        maxPara / price
+      )}</span>
+       tane
+    </p>
+  </div>
+  <div class="urun__info-container">
+    <h2>${name}</h2>
+    <p class="urun__text">Fiyat: <span id="urunFiyat">${price}</span>TL</p>
+    <p class="urun__text">Alinan: <span id="urunAlindi">0</span></p>
+    <button class="urun__ekle-btn">Satin AL</button>
+  </div>
+</div>`;
+
+  return urunElement;
+}
+function alinaBilecekRender() {
+  //Butun urunlerin secimi
+  const urunler = document.querySelectorAll(".urun");
+  //Urunlerin alinabilecek sayilarini tekrardan renderlama
+  urunler.forEach((urun) => {
+    let urunFiyat = urun.querySelector("#urunFiyat").textContent;
+    urunFiyat = Number(urunFiyat);
+    urun.querySelector("#urunAlinabilecek").textContent = Math.floor(
+      maxPara / urunFiyat
+    );
+  });
+}
+
+function urunleriEkle() {
+  //Alinan urun listesiyle urun olustur.
+  let urunlerHTML = "";
+  const urunContainer = document.getElementById("urunlerList");
+  urunlerHTML = productList.reduce((htmlDoc, product) => {
+    return htmlDoc + urunOlustur(product);
+  }, "");
+  urunContainer.innerHTML = urunlerHTML;
+}
+
+function satinAlmaKontrol(urunFiyat, btnElement) {
+  //Urunu satin alabilecek para olup olmadiginin kontrolu.
+  if (maxPara - urunFiyat >= 0) {
+    return true;
+  } else {
+    //Para olmamasi durumunda butonun degistirilmesi.
+    btnElement.disabled = true;
+    btnElement.textContent = "Bu para benim mazotuma yetmez";
+    return false;
+  }
+}
+
+function paraCikar(urunFiyat) {
+  //Para cikar tekrar headerda renderla
+  maxPara -= urunFiyat;
+  headerParaRender();
+}
+
+function satinAlmaEkle(tiklananUrun) {
+  //Tiklanan urunun alinan sayisini secme
+  let alinmaElement = tiklananUrun.querySelector("#urunAlindi");
+  let alinmaCount = 0;
+  //Alinma sayisini arttirip tekrar alinma ogesine yazma.
+  alinmaCount = alinmaElement.textContent;
+  alinmaCount++;
+  alinmaElement.textContent = alinmaCount;
+}
+
+function urunSatinAl() {
+  //Tiklanan butonun parent elementini kullanarak fiyat bilgisini alma.
+  const btnContainerElement = this.parentNode;
+  let urunFiyati = btnContainerElement.querySelector("#urunFiyat").textContent;
+
+  //Urun fiyatinin kontrolu ve toplam paradan cikarma:
+  urunFiyati = Number(urunFiyati);
+  if (satinAlmaKontrol(urunFiyati, this)) {
+    paraCikar(urunFiyati);
+    satinAlmaEkle(btnContainerElement);
+    alinaBilecekRender();
+  }
+}
+
+function buttonEventEkle() {
+  //Butun butonlara click eventi ekleme
+  document
+    .querySelectorAll(".urun__ekle-btn")
+    .forEach((btn) => btn.addEventListener("click", urunSatinAl));
+}
+
+//Loadda calistir
+headerParaRender();
+urunleriEkle();
+buttonEventEkle();
+alinaBilecekRender();
